fe06c9b854f9d2dda0c7dfa64e7db4799acef4c7
yuripierriv <yuripierriv@gmail.com>
2023-11-08 14:12:37 -0300

    botoes drop down funcionando

--- static/script.js
+++ static/script.js
@@ -3,53 +3,55 @@ document.addEventListener("DOMContentLoaded", function() {
     // Obtenha o botão "Entrar" e o menu suspenso
     var loginBtn = document.getElementById("loginBtn");
     var loginDropdown = document.getElementById("loginDropdown");
-    var duvidasBtn = document.getElementsByClassName("btn-duvidas");
 
 
-    for (var i = 0; i < duvidasBtn.length; i++) {
-        duvidasBtn[i].addEventListener("click", function(e) {
-            // Obter os elementos atualmente abertos
-            var openElements = document.querySelectorAll(".texto-oculto:not([style*='display: none'])");
-            var icon = this.getElementsByClassName("icon-btn");
-    
-            // Fechar os elementos atualmente abertos
-            
-        
-            // Abrir os elementos associados a este botão
-            var child = this.getElementsByClassName("texto-oculto");
-            
-    
-            for (var j = 0; j < icon.length;j++){
-                if(icon[j].classList.contains("bi-chevron-down")){
-                    icon[j].classList.replace("bi-chevron-down","bi-chevron-up");
-                    continue;
+    const buttons = document.querySelectorAll('.btn-duvidas');
+
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                // Fecha todos os textos ocultos
+                const textoId = `texto-${button.id}`;
+                const textoOculto = document.getElementById(textoId);
+                const iconBtn = button.querySelector('.icon-btn');
+
+                if (window.getComputedStyle(textoOculto).getPropertyValue("display") === 'block') {
+                    console.log(window.getComputedStyle(textoOculto).getPropertyValue("display"));
+                    textoOculto.style.display = 'none';
+                    iconBtn.classList.remove('bi-chevron-up');
+                    iconBtn.classList.add('bi-chevron-down');
+                    document.querySelectorAll('.texto-oculto').forEach(texto => {
+                        texto.style.display = 'none';
+                    });
+                } else {
+                    document.querySelectorAll('.texto-oculto').forEach(texto => {
+                        if(texto.id != textoId){
+                            texto.style.display = 'none';
+                            console.log(window.getComputedStyle(textoOculto).getPropertyValue("display"));
+                        }
+                        
+                    });
+                    console.log(window.getComputedStyle(textoOculto).getPropertyValue("display"));
+                    textoOculto.style.display = 'block';
+                    iconBtn.classList.remove('bi-chevron-down');
+                    console.log(window.getComputedStyle(textoOculto).getPropertyValue("display"));
+                    iconBtn.classList.add('bi-chevron-up');
+                    
+
                 }
-                if(icon[j].classList.contains("bi-chevron-up")){
-                    icon[j].classList.replace("bi-chevron-up","bi-chevron-down");
-                    continue;
-                }
-            }
-    
-        
-            for (var j = 0; j < child.length; j++) {
-                var currentDisplay = window.getComputedStyle(child[j]).getPropertyValue("display");
-            
-                if(currentDisplay == "none"){
-                    child[j].style.display = "block";
-                    continue;
-                }
-                if(currentDisplay == "block"){
-                    child[j].style.display = "none";
-                    continue;
-                }
-            }
+
+                
+
+                // Redefine todos os ícones para baixo
+                document.querySelectorAll('.icon-btn').forEach(icon => {
+                    icon.classList.remove('bi-chevron-up');
+                    icon.classList.add('bi-chevron-down');
+                });
+
+                
+
+                
+            });
         });
-
-        for (var j = 0; j < openElements.length; j++) {
-            openElements[j].style.display = "none";
-            
-        }
-    }
 
 
 
